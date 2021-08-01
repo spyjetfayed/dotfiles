67a369e365c48873c47d4d33045c6383b939ce0c
daikky <spixy@tixy>
2021-08-01 20:27:42 +0530

    basic 2020-2030 decade countdown timer script

---
+++ waybar/decade20.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# dependency: zsh 
+
+present=$(date '+%s')
+past=$(date --date='2020-01-01' '+%s')
+future=$(date --date='2030-01-01' '+%s')
+elapsed=$(( present - past ))
+total=$(( future - past ))
+percent=$(( $(( elapsed * 100.0 )) / total ))
+printf %.4f "$percent"
