48fc36480962db48b394655e7af70676d31e56b1
daikky <spixy@tixy>
2021-08-04 20:28:35 +0530

    replaced 0 with 0 for shell math calculations

--- waybar/decade20.sh
+++ waybar/decade20.sh
@@ -6,5 +6,5 @@ past=$(date --date='2020-01-01' '+%s')
 future=$(date --date='2030-01-01' '+%s')
 elapsed=$(( present - past ))
 total=$(( future - past ))
-percent=$(( $(( elapsed * 100.0 )) / total ))
+percent=$[ ($elapsed * 100.0)  / total ]
 printf %.4f "$percent"
